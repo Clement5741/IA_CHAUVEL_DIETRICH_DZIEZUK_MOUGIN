55f015c33fc790d23ea0a924e4379e3247950885
CocoDietrich <corentindietrich57@gmail.com>
2025-01-15 14:22:47 +0100

    Ajout MinMax

---
+++ src/ia/algo/jeux/MinMaxPlayer.java
@@ -0,0 +1,83 @@
+package ia.algo.jeux;
+
+import ia.framework.common.Action;
+import ia.framework.common.ActionValuePair;
+import ia.framework.jeux.Game;
+import ia.framework.jeux.GameState;
+import ia.framework.jeux.Player;
+
+import java.util.ArrayList;
+
+public class MinMaxPlayer extends Player {
+
+    private static final int DEFAULT_DEPTH = Integer.MAX_VALUE;
+    private final int depth;
+
+    /**
+     * Represente un joueur
+     *
+     * @param g            l'instance du jeux
+     * @param player_one   si joueur 1
+     * @param valueOfParam la profondeur maximale à explorer
+     */
+    public MinMaxPlayer(Game g, boolean player_one, int valueOfParam) {
+        super(g, player_one);
+        if (valueOfParam == -1) {
+            depth = DEFAULT_DEPTH;
+        } else {
+            depth = valueOfParam;
+        }
+    }
+
+    @Override
+    public Action getMove(GameState state) {
+        int player = state.getPlayerToMove();
+        if (player == 1) {
+            return maxValue(state, depth).getAction();
+        } else {
+            return minValue(state, depth).getAction();
+        }
+    }
+
+    private ActionValuePair maxValue(GameState state, int depth) {
+        if (state.isFinalState() || depth == 0) {
+            return new ActionValuePair(null, state.getGameValue());
+        }
+
+        double maxVal = Double.NEGATIVE_INFINITY;
+        Action bestAction = null;
+        ArrayList<Action> actions = game.getActions(state);
+
+        for (Action action : actions) {
+            GameState nextState = (GameState) game.doAction(state, action);
+            double value = minValue(nextState, depth - 1).getValue();
+            if (value > maxVal) {
+                maxVal = value;
+                bestAction = action;
+            }
+        }
+
+        return new ActionValuePair(bestAction, maxVal);
+    }
+
+    private ActionValuePair minValue(GameState state, int depth) {
+        if (state.isFinalState() || depth == 0) {
+            return new ActionValuePair(null, state.getGameValue());
+        }
+
+        double minVal = Double.POSITIVE_INFINITY;
+        Action bestAction = null;
+        ArrayList<Action> actions = game.getActions(state);
+
+        for (Action action : actions) {
+            GameState nextState = (GameState) game.doAction(state, action);
+            double value = maxValue(nextState, depth - 1).getValue();
+            if (value < minVal) {
+                minVal = value;
+                bestAction = action;
+            }
+        }
+
+        return new ActionValuePair(bestAction, minVal);
+    }
+}
