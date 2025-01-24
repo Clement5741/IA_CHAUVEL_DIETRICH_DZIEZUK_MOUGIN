3f16101db133e7c9ad853e65f024781b1e627c3e
CocoDietrich <corentindietrich57@gmail.com>
2025-01-24 09:22:01 +0100

    MLPTest final

--- src/mlp/MLPTest.java
+++ src/mlp/MLPTest.java
@@ -1,16 +1,33 @@
 package mlp;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
 
 public class MLPTest {
 
+    /**
+     * Ce programme teste un perceptron multicouche (MLP) sur les tables logiques AND, OR, XOR.
+     *
+     * Arguments possibles :
+     * -func {sigmoid|tanh}  : Spécifie la fonction de transfert à utiliser.
+     * -table {and|or|xor|andor}   : Spécifie la table logique à tester.
+     * -rate {valeur}        : Définit le taux d'apprentissage.
+     * -nb {valeur}          : Définit le nombre maximal d'itérations.
+     * -layers {x,y,z,...}   : Spécifie les couches du réseau (exemple : 2,2,1).
+     * -err {valeur}         : Définit l'erreur cible pour chaque exemple.
+     */
     public static void main(String[] args) {
         // Paramètres par défaut
         String functionType = "sigmoid";
-        String tableType = "xor";
+        String tableType = "and";
         double learningRate = 0.1;
+        int maxIterations = 10000;
+        int[] layers = {2, 2, 1};
+        double errorTarget = 0.01;
 
-        // Récupérer les arguments
+        // Traiter les arguments de ligne de commande
         for (int i = 0; i < args.length; i++) {
             switch (args[i]) {
                 case "-func":
@@ -21,6 +38,19 @@ public class MLPTest {
                     break;
                 case "-rate":
                     learningRate = Double.parseDouble(args[++i]);
+                    break;
+                case "-nb":
+                    maxIterations = Integer.parseInt(args[++i]);
+                    break;
+                case "-layers":
+                    String[] layerParts = args[++i].split(",");
+                    layers = new int[layerParts.length];
+                    for (int j = 0; j < layerParts.length; j++) {
+                        layers[j] = Integer.parseInt(layerParts[j]);
+                    }
+                    break;
+                case "-err":
+                    errorTarget = Double.parseDouble(args[++i]);
                     break;
                 default:
                     System.out.println("Argument inconnu : " + args[i]);
@@ -39,8 +69,9 @@ public class MLPTest {
         double[][] outputAnd = {{0}, {0}, {0}, {1}};
         double[][] outputOr = {{0}, {1}, {1}, {1}};
         double[][] outputXor = {{0}, {1}, {1}, {0}};
+        double[][] outputAndOr = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
 
-        // Sélectionner la table de vérité
+        // Sélectionner la table logique
         double[][] outputData;
         switch (tableType) {
             case "and":
@@ -48,6 +79,9 @@ public class MLPTest {
                 break;
             case "or":
                 outputData = outputOr;
+                break;
+            case "andor":
+                outputData = outputAndOr;
                 break;
             case "xor":
             default:
@@ -63,54 +97,61 @@ public class MLPTest {
             function = new SigmoidFunction();
         }
 
-        // Configuration du réseau
-        int[] layers = {2, 2, 1};
-
         // Lancer le test
-        System.out.println("Test avec la fonction : " + functionType + ", Table : " + tableType + ", Taux : " + learningRate);
-        testMLP(inputData, outputData, layers, learningRate, function);
+        System.out.println("Test avec la fonction : " + functionType + ", Table : " + tableType + ", Taux : " + learningRate + ", Itérations : " + maxIterations + ", Erreur cible : " + errorTarget);
+        testMLP(inputData, outputData, layers, learningRate, function, maxIterations, errorTarget);
     }
 
-    private static void testMLP(double[][] inputData, double[][] outputData, int[] layers, double learningRate, TransferFunction function) {
+    private static void testMLP(double[][] inputData, double[][] outputData, int[] layers, double learningRate,
+                                TransferFunction function, int maxIterations, double errorTarget) {
         // Initialiser le perceptron
         MLP mlp = new MLP(layers, learningRate, function);
 
-        // Configuration de l'apprentissage
-        int maxIterations = 10000;
-        double errorThreshold = 0.01;
+        // Stocker les erreurs et sorties finales par ligne
+        double[] finalErrors = new double[inputData.length];
+        double[][] finalOutputs = new double[inputData.length][];
+
+        // Mélanger les données
+        List<Integer> indices = new ArrayList<>();
+        for (int i = 0; i < inputData.length; i++) {
+            indices.add(i);
+        }
 
         for (int iteration = 0; iteration < maxIterations; iteration++) {
-            double totalError = 0.0;
+            boolean allBelowTarget = true;
 
-            // Mélanger les données d'entrée
-            for (int i = 0; i < inputData.length; i++) {
-                int index = (int) (Math.random() * inputData.length);
+            // Mélanger les indices à chaque itération
+            Collections.shuffle(indices);
+
+            for (int index : indices) {
                 double[] input = inputData[index];
                 double[] output = outputData[index];
 
-                // Apprentissage par rétropropagation
-                totalError += mlp.backPropagate(input, output);
+                // Apprentissage par rétropropagation et mise à jour des erreurs finales
+                double error = mlp.backPropagate(input, output);
+                finalErrors[index] = error;
+                finalOutputs[index] = mlp.execute(input); // Stocker la sortie finale
+
+                // Vérifier si l'erreur dépasse le seuil cible
+                if (error > errorTarget) {
+                    allBelowTarget = false;
+                }
             }
 
-            // Afficher l'erreur moyenne périodiquement
-            if (iteration % 100 == 0) {
-                System.out.println("Itération " + iteration + ", Erreur moyenne : " + (totalError / inputData.length));
-            }
-
-            // Vérifier si l'apprentissage est terminé
-            if (totalError / inputData.length < errorThreshold) {
-                System.out.println("Apprentissage terminé après " + iteration + " itérations.");
+            // Vérifier si toutes les erreurs sont en dessous du seuil cible
+            if (allBelowTarget) {
+                System.out.println("Toutes les erreurs sont en dessous de " + errorTarget + " après " + iteration + " itérations.");
                 break;
             }
         }
 
-        // Tester les performances
+        // Afficher les résultats finaux pour chaque ligne
         System.out.println("\nRésultats après apprentissage :");
         for (int i = 0; i < inputData.length; i++) {
             double[] input = inputData[i];
-            double[] predictedOutput = mlp.execute(input);
             System.out.println("Entrée : " + Arrays.toString(input) +
-                    " -> Sortie prédite : " + Arrays.toString(predictedOutput));
+                    " -> Sortie finale : " + Arrays.toString(finalOutputs[i]) +
+                    ", Erreur finale : " + finalErrors[i]);
         }
     }
 }
