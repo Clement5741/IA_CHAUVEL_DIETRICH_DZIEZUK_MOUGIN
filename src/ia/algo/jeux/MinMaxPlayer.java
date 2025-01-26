a33edbe7ef7892abdb7836e54107c6b24bee50d0
Clement5741 <clement.chauvel1@etu.univ-lorraine.fr>
2025-01-26 18:04:01 +0100

    Modif MinMaxPlayer

--- src/ia/algo/jeux/MinMaxPlayer.java
+++ src/ia/algo/jeux/MinMaxPlayer.java
@@ -6,84 +6,72 @@ import ia.framework.jeux.Game;
 import ia.framework.jeux.GameState;
 import ia.framework.jeux.Player;
 
-import java.util.ArrayList;
-
 public class MinMaxPlayer extends Player {
 
-    private static final int DEFAULT_DEPTH = Integer.MAX_VALUE;
-    private final int depth;
+    private final int maxDepth;
 
     /**
      * Represente un joueur
      *
      * @param g            l'instance du jeux
      * @param player_one   si joueur 1
-     * @param valueOfParam la profondeur maximale à explorer
+     * @param maxDepth la profondeur maximale à explorer
      */
-    public MinMaxPlayer(Game g, boolean player_one, int valueOfParam) {
+    public MinMaxPlayer(Game g, boolean player_one, int maxDepth) {
         super(g, player_one);
-        if (valueOfParam == -1) {
-            depth = DEFAULT_DEPTH;
-        } else {
-            depth = valueOfParam;
-        }
+        name = "MinMax";
+        this.maxDepth = maxDepth;
     }
 
     @Override
     public Action getMove(GameState state) {
-        int player = state.getPlayerToMove();
-        Action action;
-        if (player == 1) {
-            action = maxValue(state, depth).getAction();
+        if (this.player == PLAYER1) {
+            return maxValue(state, 0).getAction();
         } else {
-            action = minValue(state, depth).getAction();
+            return minValue(state, 0).getAction();
         }
-        return action;
     }
 
     private ActionValuePair maxValue(GameState state, int depth) {
-        this.incStateCounter(); // Incrémenter le compteur
+        this.incStateCounter();
 
-        if (state.isFinalState() || depth == 0) {
+        if (game.endOfGame(state) || depth >= maxDepth) {
             return new ActionValuePair(null, state.getGameValue());
         }
 
-        double maxVal = Double.NEGATIVE_INFINITY;
-        Action bestAction = null;
-        ArrayList<Action> actions = game.getActions(state);
+        double vMax = Double.NEGATIVE_INFINITY;
+        Action cMax = null;
 
-        for (Action action : actions) {
+        for (Action action : game.getActions(state)) {
             GameState nextState = (GameState) game.doAction(state, action);
-            double value = minValue(nextState, depth - 1).getValue();
-            if (value > maxVal) {
-                maxVal = value;
-                bestAction = action;
+            ActionValuePair value = minValue(nextState, depth + 1);
+            if (value.getValue() >= vMax) {
+                vMax = value.getValue();
+                cMax = action;
             }
         }
 
-        return new ActionValuePair(bestAction, maxVal);
+        return new ActionValuePair(cMax, vMax);
     }
 
     private ActionValuePair minValue(GameState state, int depth) {
-        this.incStateCounter(); // Incrémenter le compteur
-
-        if (state.isFinalState() || depth == 0) {
+        this.incStateCounter();
+        if (game.endOfGame(state) || depth >= maxDepth) {
             return new ActionValuePair(null, state.getGameValue());
         }
 
-        double minVal = Double.POSITIVE_INFINITY;
-        Action bestAction = null;
-        ArrayList<Action> actions = game.getActions(state);
+        double vMin = Double.POSITIVE_INFINITY;
+        Action cMin = null;
 
-        for (Action action : actions) {
+        for (Action action : game.getActions(state)) {
             GameState nextState = (GameState) game.doAction(state, action);
-            double value = maxValue(nextState, depth - 1).getValue();
-            if (value < minVal) {
-                minVal = value;
-                bestAction = action;
+            ActionValuePair value = maxValue(nextState, depth + 1);
+            if (value.getValue() <= vMin) {
+                vMin = value.getValue();
+                cMin = action;
             }
         }
 
-        return new ActionValuePair(bestAction, minVal);
+        return new ActionValuePair(cMin, vMin);
     }
 }
