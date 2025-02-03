866c0b88e452d7dbd61cb85e9d394023e752db51
Clement5741 <clement.chauvel1@etu.univ-lorraine.fr>
2025-02-03 20:25:47 +0100

    Revert "Ajout test jeux et fonction d'évaluation Mnk"
    This reverts commit 8e47dac29e663f0a8fd05981bfaadd4d2574a366.

--- src/ia/problemes/MnkGameState.java
+++ src/ia/problemes/MnkGameState.java
@@ -2,7 +2,6 @@ package ia.problemes;
 
 import java.util.Arrays;
 import java.util.ArrayList;
-import java.util.function.Function;
 
 import ia.framework.common.Action;
 import ia.framework.common.State;
@@ -48,98 +47,10 @@ public class MnkGameState extends AbstractMnkGameState {
      * 
      * @return la valeur du jeux
      **/
-//    protected double evaluationFunction() {
-//        return Double.NaN;
-//    }
-    @Override
     protected double evaluationFunction() {
-        int scoreX = calculateDangerScore(X);
-        int scoreO = calculateDangerScore(O);
-
-        return scoreX - scoreO;
+        return Double.NaN;
     }
+    
 
-    // Fonction pour évaluer le score avec notion de danger
-    private int calculateDangerScore(int player) {
-        int score = 0;
-        score += evaluateHorizontalDanger(player);
-        score += evaluateVerticalDanger(player);
-        score += evaluateDiagonalDanger(player);
-        return score;
-    }
-
-    // Évaluation du danger sur les lignes horizontales
-    private int evaluateHorizontalDanger(int player) {
-        int res = 0;
-        for (int r = 0; r < this.rows; r++) {
-            for (int c = 0; c <= this.cols - this.streak; c++) {
-                int count = 0, empty = 0;
-                for (int k = 0; k < this.streak; k++) {
-                    int value = this.getValueAt(r, c + k);
-                    if (value == player) count++;
-                    else if (value == EMPTY) empty++;
-                }
-                if (empty + count == this.streak) {
-                    res += Math.pow(2, count); // Augmentation exponentielle en fonction du danger
-                }
-            }
-        }
-        return res;
-    }
-
-    // Évaluation du danger sur les lignes verticales
-    private int evaluateVerticalDanger(int player) {
-        int res = 0;
-        for (int c = 0; c < this.cols; c++) {
-            for (int r = 0; r <= this.rows - this.streak; r++) {
-                int count = 0, empty = 0;
-                for (int k = 0; k < this.streak; k++) {
-                    int value = this.getValueAt(r + k, c);
-                    if (value == player) count++;
-                    else if (value == EMPTY) empty++;
-                }
-                if (empty + count == this.streak) {
-                    res += Math.pow(2, count);
-                }
-            }
-        }
-        return res;
-    }
-
-    // Évaluation du danger sur les diagonales
-    private int evaluateDiagonalDanger(int player) {
-        int res = 0;
-
-        // Diagonale descendante (\)
-        for (int r = 0; r <= this.rows - this.streak; r++) {
-            for (int c = 0; c <= this.cols - this.streak; c++) {
-                int count = 0, empty = 0;
-                for (int k = 0; k < this.streak; k++) {
-                    int value = this.getValueAt(r + k, c + k);
-                    if (value == player) count++;
-                    else if (value == EMPTY) empty++;
-                }
-                if (empty + count == this.streak) {
-                    res += Math.pow(2, count);
-                }
-            }
-        }
-
-        // Diagonale montante (/)
-        for (int r = this.streak - 1; r < this.rows; r++) {
-            for (int c = 0; c <= this.cols - this.streak; c++) {
-                int count = 0, empty = 0;
-                for (int k = 0; k < this.streak; k++) {
-                    int value = this.getValueAt(r - k, c + k);
-                    if (value == player) count++;
-                    else if (value == EMPTY) empty++;
-                }
-                if (empty + count == this.streak) {
-                    res += Math.pow(2, count);
-                }
-            }
-        }
-        return res;
-    }
-
+    
 }
